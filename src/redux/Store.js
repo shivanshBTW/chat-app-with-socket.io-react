f384bad888d924085df90e6d281683487c18e41c
shivanshBTW <stshivansh9@gmail.com>
2020-01-08 18:10:17 +0530

    wip socket

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,1 +1,12 @@
 import React from 'react'
+import {Provider} from "react-redux";
+
+const CTX = React.createContext();
+
+export default function Store() {
+   return (
+      <CTX.Provider value={}>
+         {props.children}
+      </CTX.Provider>
+   )
+}
